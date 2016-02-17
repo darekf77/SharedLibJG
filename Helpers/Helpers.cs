1168b42251f3152e744cc0d350dafd877b866405
Darek <darekf77@gmail.com>
2016-02-17 14:56:26 +0100

    new game ui

---
+++ Helpers/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibJG.Helpers
+{
+    public class Helpers
+    {
+        [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
+        public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
+    }
+}
