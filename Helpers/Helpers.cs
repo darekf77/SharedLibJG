2bae8a653eda920fc144c275fb3a423e57f015c7
Darek <darekf77@gmail.com>
2016-02-21 11:50:01 +0100

    fix retina

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -11,5 +12,20 @@ namespace SharedLibJG.Helpers
     {
         [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
         public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
+
+        private const float _defaultDPI = 96;
+
+
+        static Bitmap tmpb = new Bitmap(1, 1);
+        static Graphics gg = Graphics.FromImage(tmpb);
+
+
+
+        public static int getResponsiveFontSize(int fontsize)
+        {
+            float dpi = Math.Min(gg.DpiX, gg.DpiY);
+            return (int)(_defaultDPI * (float)fontsize / dpi);
+        }
+
     }
 }
